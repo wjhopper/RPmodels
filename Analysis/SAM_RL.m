2c0a1542af61e4ac9bf6ead7b934493c7831cc31
whopper <wjhopper510@gmail.com>
2014-04-29 11:09:58 -0400

    Vectorized the study sampling, and allow only one recpvery attempt per item

---
+++ Analysis/SAM_RL.m
@@ -0,0 +1,17 @@
+% SAM Testing Modeling
+% Parameters
+S_one=.1; % Sampling Strength after initial study
+S_two=3; % Sampling Strength after round of study practice
+S_correct=S_two; % Sampling Strength after correct recall in test practice - same as after study practice
+S_incorrect=S_one; % Sampling strength after incorrect recall in test practice - same as after initial study i.e. no sampling learning
+R_one=1; % Recovery Strength after initial study
+R_two=R_one; % Recovery Strength after study practice - same as after initial study i.e. no recovery learning
+R_correct=10; % Recovery Strength after correct recall
+R_incorrect=R_one; % Recovery Strength after incorrect recall
+O_imm=1; % Extralist interference at test practice and at 5 minute final test - fixed
+O_two=5; % Extralist interference at 2 day test
+O_seven=10; % Extralist interference at 7 day test
+
+params=[S_one S_two R_one R_correct O_two O_seven];
+[fitted_params, chisquare, ~ , info]=fminsearch(@fit_SAM,params,optimset('MaxFunEvals',1500));
+%fit_SAM(params);
---
+++ Analysis/fit_SAM.m
@@ -0,0 +1,103 @@
+function  err = fit_SAM(params)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+% data=[acc on recall practice, acc on 5 min test after study, acc on 5 min
+% test after recall practice, acc on 2 day test after study, acc on 2 day
+% test after recall practice, acc on 1 week test after study, acc on 1 week
+% test after recall practice]
+data=[.70,.81,.75,.54,.68,.42,.56];
+k=100;
+nItems=30;
+O_imm=1;
+S_one=params(1);
+S_two=params(2);
+R_one=params(3);
+R_correct=params(4);
+O_two=params(5);
+O_seven=params(6);
+
+p_sample_study=[S_one S_two S_two S_two]./([S_one, S_two S_two S_two]*30  + [O_imm O_imm O_two O_seven]);
+p_recover=[R_one R_correct]./([R_one R_correct]+O_imm);
+
+
+% After initial Study
+p_sample_practice=1-((1-p_sample_study(1))^k);
+p_recall_practice=p_sample_practice*p_recover(1);
+%p_lost=p_sample_practice*(1-p_recover(1));
+
+%After 5 mins
+%Studied Items
+p_sample_study_imm=1-((1-p_sample_study(2))^k);
+p_recall_study_imm=p_sample_study_imm*p_recover(1);
+
+%Tested Items
+p_sample_test_correct_imm=S_two/((S_two*floor(p_recall_practice*nItems))...
+                                +(S_one*ceil((1-p_recall_practice)*nItems))+O_imm);
+p_sample_test_incorrect_imm=S_one/((S_two*floor(p_recall_practice*nItems))...
+                             +(S_one*ceil((1-p_recall_practice)*nItems))+O_imm);    
+%p_sample_test_imm=(floor(p_recall_practice*nItems)/nItems)*(1-((1-p_sample_test_correct_imm)^k))...
+%                    + ((ceil((1-p_recall_practice)*nItems)/nItems)*(1-((1-p_sample_test_incorrect_imm)^k)));                         
+p_recall_test_imm=(((floor(p_recall_practice*nItems)/nItems)*(1-((1-p_sample_test_correct_imm)^k)))*p_recover(2))...
+                  + ((ceil((1-p_recall_practice)*nItems)/nItems)*((1-((1-p_sample_test_incorrect_imm)^k))*0));
+
+% After 2 days
+%Studied Items
+p_sample_study_two=1-((1-p_sample_study(3))^k);
+p_recall_study_two=p_sample_study_two*p_recover(1);
+%Tested Items
+p_sample_test_correct_two=S_two/((S_two*floor(p_recall_practice*nItems))...
+                           +(S_one*ceil((1-p_recall_practice)*nItems))+O_two);
+p_recall_test_correct_two=(1-((1-p_sample_test_correct_two)^k))*p_recover(2);
+p_sample_test_incorrect_two=S_one/((S_two*floor(p_recall_practice*nItems))...
+                             +(S_one*ceil((1-p_recall_practice)*nItems))+O_two);
+p_recall_test_incorrect_two=(1-((1-p_sample_test_incorrect_two)^k))*p_recover(1);
+p_recall_test_two= ((floor(p_recall_practice*nItems)/nItems*p_recall_test_correct_two))...
+                    + ((ceil((1-p_recall_practice)*nItems)/nItems)*p_recall_test_incorrect_two);
+               
+
+%After 7 days
+%Studied Itesm
+p_sample_study_seven=1-((1-p_sample_study(4))^k);
+p_recall_study_seven=p_sample_study_seven*p_recover(1);
+%Tested Items
+p_sample_test_correct_seven=S_two/((S_two*floor(p_recall_practice*nItems))...
+                           +(S_one*ceil((1-p_recall_practice)*nItems))+O_seven);
+p_recall_test_correct_seven=(1-((1-p_sample_test_correct_seven)^k))*p_recover(2);
+p_sample_test_incorrect_seven=S_one/((S_two*floor(p_recall_practice*nItems))...
+                             +(S_one*ceil((1-p_recall_practice)*nItems))+O_seven);
+p_recall_test_incorrect_seven=(1-((1-p_sample_test_incorrect_seven)^k))*p_recover(1);
+p_recall_test_seven= ((floor(p_recall_practice*nItems)/nItems*p_recall_test_correct_seven))...
+                    + ((ceil((1-p_recall_practice)*nItems)/nItems)*p_recall_test_incorrect_seven);
+                
+                
+
+pred_tot=[p_recall_practice p_recall_study_imm p_recall_test_imm ...
+    p_recall_study_two p_recall_test_two p_recall_study_seven p_recall_test_seven];
+%p_sample=[p_sample_one p_sample_study_imm
+
+Lu=(data.*log(data))+((1-data).*log(1-data));
+Lc=(data.*log(pred_tot))+((1-data).*log(1-pred_tot));
+err=-sum((2*nItems*(Lc-Lu)));
+
+if (S_one <=0 || S_two <= 0 || R_one <= 0 || R_correct <= 0 || O_two <= 0 || O_seven <= 0)
+    err=1000000;
+end
+
+if S_two < S_one;
+   err=1000000;
+end 
+
+% if R_two < R_one;
+%    err=1000000;
+% end 
+if R_correct < R_one;
+   err=1000000;
+end 
+
+figure(1);
+hold off
+plot(data);
+hold on
+plot(pred_tot,'r');
+
+
