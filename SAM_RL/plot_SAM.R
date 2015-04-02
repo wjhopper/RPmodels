e4c1f4e552e5464a91a90501bd761b49ea98bf8b
whopper-cemnl <wjhopper510@gmail.com>
2015-04-02 10:19:41 -0400

    re-write of the plotting function to use ggplot, and use the SAMRI function to generate predictions instead of doing both generating predictions and plotting

--- SAM_RL/plot_SAM.R
+++ SAM_RL/plot_SAM.R
@@ -1,107 +1,134 @@
-O<-seq(1,10,.1)
-k=500
-nItems=30
+library(reshape2)
+library(ggplot2)
 
-params<-read.csv(file.choose(),header=F,
-                 col.names=c('S1','S2','R1','R2','O2','O7'))
+# Rows 1 and 2 will do the CEMS poster data 
+
+## Get fits 
+## @knitr get_fits
+mytheme <- theme(axis.title.x=element_text(size=rel(2),vjust=-.65),
+                 axis.title.y=element_text(size=rel(2),vjust=1.5),
+                 axis.title.x=element_text(size=rel(2)),
+                 axis.title.y=element_text(size=rel(2)),
+                 axis.ticks = element_line(size=rel(2)),
+                 axis.text = element_text(size=rel(1.25)),
+                 legend.text = element_text(size=16),
+                 legend.title = element_text(size=20),
+                 plot.title = element_text(size=24))
+source('SAMRI.R')
+params<-read.csv(file.path(getwd(),'params.csv'),header=T)
+params=cbind(params, list(k=rep(500, nrow(params))),list(nItems=rep(30, nrow(params))))
 good<-unlist(params[1,])
 bad<-unlist(params[2,])
-data<-data.frame(acc=c(.70,.81,.75,.54,.68,.42,.56),
-                 time=c(1,1,1,2.9828,2.9828,4.499,4.499),
-                 group=c('test','study','test','study','test','study','test')
+badbutgood<-unlist(params[3,])
+obs=c(.70,.81,.75,.54,.68,.42,.56)
+data<-data.frame(acc=rep(obs,nrow(params)),
+                 time=c(1,1,1,params[1,'O2'],params[1,'O2'],params[1,'O7'],params[1,'O7'],
+                        1,1,1,params[2,'O2'],params[2,'O2'],params[2,'O7'],params[2,'O7'],
+                        1,1,1,params[3,'O2'],params[3,'O2'],params[3,'O7'],params[3,'O7']),
+                 group=rep(c('test','study','test','study','test','study','test'),nrow(params)),
+                 Test_Type=rep(c("Practice Test", "Final Test","Final Test","Final Test","Final Test","Final Test","Final Test"),nrow(params)),
+                 class=rep(c("OneStudy", "TwoStudy","Test","TwoStudy","Test","TwoStudy","Test"),nrow(params)),
+                 paramset=rep(c(1,2,3),each=length(obs))
                 )
-## good version, without second chance
-par(mar=c(5,5,3,1))
-plot(data[data$group=='test','time'][2:4],data[data$group=='test','acc'][2:4],
-     lwd=3,pch=4,col='red',ylim=c(.35,.9),xlim=c(1,10),
-     ylab="Recall Accuracy",xlab="Interference (O) / Retention Interval (days)",
-     cex.lab=2.5,cex.main=2,main="Observed Data (Roediger & Karpicke, 2006) & SAM-RL Model Predictions")
-points(data[data$group=='test','time'][1],data[data$group=='test','acc'][1],
-       lwd=3,pch=2,col='blue')
-points(data[data$group=='study','time'],data[data$group=='study','acc'],
-       lwd=3,pch=5,col='green')
-legend("topright",legend=c("Practice Test","Final Test (Restudy)","Final Test (Testing)","Model Predictions (No Extra Practice)","Model Predictions (Restudy)","Model Predictions (Testing)")
-       ,col=c('blue','green','red','blue','green','red'),lwd=2,pch=c(2,5,4,NA,NA,NA),lty=c(NA,NA,NA,2,2,2),cex=2)
 
-p_sample_study=data.frame('S1'=good[1]/((good[1]*30)+O),'S2'=good[2]/((good[2]*30)+O))
-p_recover=data.frame('R1'=good[3]/((good[3])+O),'R2'=good[4]/((good[4])+O))
+point_fits <- SAMRI(params[1,], t=c(1,params[1,'O2'],params[1,'O7']), one_shot='mixed',predict=FALSE)
+melted_fits <- melt(point_fits, id =c("time","one_shot"),variable.name="class",value.name="acc")
+df <- data[data$paramset==1,c('acc','time','class')]
+df$type <- 'real'
+melted_fits$one_shot <- NULL
+melted_fits$type <- 'model'
+melted_fits <- melted_fits[!(melted_fits$time > 1 & melted_fits$class=='OneStudy'),]
+df <- rbind(melted_fits,df)
+df$time[df$time==1] <- '5 Minutes'
+df$time[df$time == '2.9828'] <- '2 Days'
+df$time[df$time=='4.499'] <- '7 Days'
+df$time <- factor(df$time,levels= c('5 Minutes','2 Days', '7 Days'))
 
-
-p_sample_practice=1-((1-p_sample_study[1,1])^k)
-p_recall_practice=p_sample_practice*p_recover[1,1]
-practice_sampling <-1-((1-p_sample_study[,1])^k)
-practice_forgetting<-practice_sampling*p_recover[,1]
-
-
-p_sample_study_final=1-((1-p_sample_study[,2])^k)
-p_recall_study_final=p_sample_study_final*p_recover[,1]
-
-#Test Condition
-#Correct Items
-p_sample_test_correct=good[2]/((good[2]*p_recall_practice*nItems)
-                              +(good[1]*(1-p_recall_practice)*nItems)+O);
-p_recall_test_correct=(1-((1-p_sample_test_correct))^k)*p_recover[,2]
-
-#Incorrect Items
-p_sample_test_incorrect=good[1]/((good[2]*(p_recall_practice*nItems))
-                                +(good[1]*(1-p_recall_practice)*nItems)+O)
-p_recall_test_incorrect=(1-((1-p_sample_test_incorrect))^k)*p_recover[,1]
-#Recall on immediate
-p_recall_test_imm=(p_recall_test_correct*p_recall_practice) + (p_recall_test_incorrect*(1-p_sample_practice));
-#Recall on delayed tests
-p_recall_test_delay=(p_recall_test_correct*p_recall_practice) + (p_recall_test_incorrect*(1-p_recall_practice));
-
-#plot the predictions
-lines(O,c(p_recall_test_imm[1:2],p_recall_test_delay[3:91]),lty=2,lwd=2,col='red')
-lines(O,p_recall_study_final,lty=2,lwd=2,col='green')
-lines(O,practice_forgetting,lty=2,lwd=2,col='blue')
+preds_same_c <- SAMRI(params[1,], one_shot='never')
+preds_same_c <- melt(preds_same_c , id =c("time","one_shot"),variable.name="class",value.name="acc")
+preds_diff_c <- SAMRI(params[1,],  one_shot='always')[,c('time','Test',"one_shot")]
+preds_diff_c <- melt(preds_diff_c , id =c("time","one_shot"),variable.name="class",value.name="acc")
+preds_mixed_c <- SAMRI(params[1,], one_shot='mixed')[,c('time','Test',"one_shot")]
+preds_mixed_c <- melt(preds_mixed_c , id =c("time","one_shot"),variable.name="class",value.name="acc")
+preds <- rbind(preds_same_c,preds_diff_c,preds_mixed_c)
+preds$one_shot <- factor(preds$one_shot,levels = c('never', 'always','mixed'))
+# preds2 <- SAMRI(params[3,], t=seq(1,5,.1))
+# comb_preds= rbind(preds_same_c,preds_diff_c,preds_mixed_c)
+# melted_preds = melt(comb_preds, id =c("time","one_shot"),variable.name="class",value.name="acc")
 
 
 
-### Bad version, with second chance
-O<-seq(1,5,.1)
-data<-data.frame(acc=c(.70,.81,.75,.54,.68,.42,.56),
-                 time=c(1,1,1,1.651246,1.651246,2.297341,2.297341),
-                 group=c('test','study','test','study','test','study','test')
-                )
-plot(data[data$group=='test','time'][2:4],data[data$group=='test','acc'][2:4],
-     lwd=3,pch=4,col='red',ylim=c(.35,.9),xlim=c(1,5),
-     ylab="Recall Accuracy",xlab="Interference (O) / Retention Interval (days)",
-     cex.lab=2.5,cex.main=2,main="Observed Data (Roediger & Karpicke, 2006) & SAM-RL Model Predictions")
-points(data[data$group=='test','time'][1],data[data$group=='test','acc'][1],
-       lwd=3,pch=2,col='blue')
-points(data[data$group=='study','time'],data[data$group=='study','acc'],
-       lwd=3,pch=5,col='green')
-legend("topright",legend=c("Practice Test","Final Test (Restudy)","Final Test (Testing)","Model Predictions (No Extra Practice)","Model Predictions (Restudy)","Model Predictions (Testing)")
-       ,col=c('blue','green','red','blue','green','red'),lwd=2,pch=c(2,5,4,NA,NA,NA),lty=c(NA,NA,NA,2,2,2),cex=2)
+## Fits only #####
+## @knitr fits_only
+p1 <- ggplot(data = df, aes(x=time, y=acc, shape = type, colour=class)) +
+      geom_point(size=5) + 
+      geom_point(size=5) + 
+      scale_color_discrete(name = "Conditions",
+                       labels = c("Practice Test", "Final Test (S)", "Final Test (T)")) +
+      scale_shape_manual(name="Source", breaks = c('model','real'), c('Model Fits', 'Obs. Data'), values = c(4,1)) + 
+      scale_x_discrete("Retention Interval") + ylab("Memory Accuracy") + 
+      ggtitle('SAM-RL vs. Observed Data') +
+      mytheme
 
-p_sample_study=data.frame('S1'=bad[1]/((bad[1]*30)+O),'S2'=bad[2]/((bad[2]*30)+O))
-p_recover=data.frame('R1'=bad[3]/((bad[3])+O),'R2'=bad[4]/((bad[4])+O))
+## always new context preds #####
+## @knitr one_new_context
+p2 <- ggplot(preds[preds$one_shot=="never",],
+             aes(time,acc,colour=class)) + 
+      geom_line(size=1.25,aes(linetype=one_shot)) + 
+      geom_point(aes(x=time, y=acc),color="black",size=4,
+                 data=data[data$paramset==1,]) +
+      geom_point(aes(x=time, y=acc, color=class),size=2.5,
+             data=data[data$paramset==1,]) +
+      xlab("Retention Interval (Days) / Interference (O)") + ylab("Memory Accuracy") + 
+      ggtitle('SAM-RL Predictions vs. Observed Data') +
+      scale_color_discrete(name = "Conditions",
+                           labels = c("Practice Test", "Final Test (S)", "Final Test (T)")) + 
+      
+      scale_linetype_manual("Final Test\nContext", 
+                        values = 3,
+                        breaks = c('never'), 
+                        labels = c("Always New Context")) + 
+      mytheme + 
+      guides(colour = guide_legend(order = 1), 
+         linetype = guide_legend(order = 2))
 
+## always old context preds #####
+## @knitr one_old_context
+p3 <- ggplot(preds[preds$one_shot %in% c("always", "never"),],
+             aes(time,acc,colour=class)) + 
+    geom_path(size=1.25,aes(linetype = one_shot)) + 
+    geom_point(aes(x=time, y=acc),color="black",size=4,
+              data=data[data$paramset==1,]) +
+    geom_point(aes(x=time, y=acc, color=class),size=2.5,
+              data=data[data$paramset==1,]) +
+    xlab("Retention Interval (Days) / Interference (O)") + ylab("Memory Accuracy") + 
+    ggtitle('SAM-RL Predictions vs. Observed Data') +
+    scale_color_discrete(name = "Conditions",
+                       labels = c("Practice Test", "Final Test (S)", "Final Test (T)")) + 
+    scale_linetype_discrete("Final Test\nContext", 
+                          breaks = c('never','always','mixed'), 
+                          labels = c("Always New Context","Always Practice Context", "Switch Context")) +
+    mytheme +
+    guides(colour = guide_legend(order = 1), 
+         linetype = guide_legend(order = 2))
+      
 
-p_sample_practice=1-((1-p_sample_study[1,1])^k)
-p_recall_practice=p_sample_practice*p_recover[1,1]
-practice_sampling <-1-((1-p_sample_study[,1])^k)
-practice_forgetting<-practice_sampling*p_recover[,1]
+## switch context preds #####
+## @knitr switch_context
+p4 <- ggplot(preds, aes(time,acc,colour=class)) + 
+  geom_path(size=1.25,aes(linetype = one_shot)) + 
+  geom_point(aes(x=time, y=acc),color="black",size=4,
+             data=data[data$paramset==1,]) +
+  geom_point(aes(x=time, y=acc, color=class),size=2.5,
+             data=data[data$paramset==1,]) +
+  xlab("Retention Interval (Days) / Interference (O)") + ylab("Memory Accuracy") + 
+  ggtitle('SAM-RL Predictions vs. Observed Data') +
+  scale_color_discrete(name = "Conditions",
+                       labels = c("Practice Test", "Final Test (S)", "Final Test (T)")) +
+  scale_linetype_discrete("Final Test\nContext", 
+                          breaks = c('never','always','mixed'), 
+                          labels = c("Always New Context","Always Practice Context", "Switch Context")) +
+  mytheme +
+  guides(colour = guide_legend(order = 1), 
+         linetype = guide_legend(order = 2))
 
-p_sample_study_final=1-((1-p_sample_study[,2])^k)
-p_recall_study_final=p_sample_study_final*p_recover[,1]
-
-#Test Condition
-#Correct Items
-p_sample_test_correct=bad[2]/((bad[2]*p_recall_practice*nItems)
-                               +(bad[1]*(1-p_recall_practice)*nItems)+O);
-p_recall_test_correct=(1-((1-p_sample_test_correct))^k)*p_recover[,2]
-
-#Incorrect Items
-p_sample_test_incorrect=bad[1]/((bad[2]*(p_recall_practice*nItems))
-                                 +(bad[1]*(1-p_recall_practice)*nItems)+O)
-p_recall_test_incorrect=(1-((1-p_sample_test_incorrect))^k)*p_recover[,1]
-
-#Recall on all tests 
-p_recall_test=(p_recall_test_correct*p_recall_practice) + (p_recall_test_incorrect*(1-p_recall_practice));
-
-#plot the predictions
-lines(O,p_recall_test,lty=2,lwd=2,col='red')
-lines(O,p_recall_study_final,lty=2,lwd=2,col='green')
-lines(O,practice_forgetting,lty=2,lwd=2,col='blue')
