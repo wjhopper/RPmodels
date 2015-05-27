9ea0ec068caa7a488da1515f0d4a889424228b56
whopper-cemnl <wjhopper510@gmail.com>
2015-05-27 09:46:30 -0400

    cleaned up stale function and m files no longer needed

--- H_and_H_LB4L/SAMRL/predict.m
+++
@@ -1,6 +0,0 @@
-function [ psampling, precovery, acc ] =predict(S,R,O,k,weight)
-    psampling = 1-((1-(S./(S+O))).^k);
-    precovery = R./(R+O);
-    acc = sum(psampling.*precovery .* weight);
-end
-
