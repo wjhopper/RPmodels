4ac0bc0ec8d8d2c3a45ff766d19c8aee0d1af94b
whopper-cemnl <wjhopper510@gmail.com>
2015-05-28 12:58:11 -0400

    fixed checking whether saved fit results exist, fixed checking on probability vs strength parameter for correct column names, and added logit SEM calculations

--- H_and_H_LB4L/SAMRL/SAMRL_sub.R
+++ H_and_H_LB4L/SAMRL/SAMRL_sub.R
@@ -1,6 +1,11 @@
 ## PLot data and SAMRL model fits 
-## @knitr readAndScore
 
+model <- list(fname = 'SAMRL_sub_results.Rdata', modelstring = "fitSub('S1',.15,'S2',.4,'R',3,'R_cor',6,'O1',1.2, 'showfigs','Off');" )
+# model <- list(fname = 'SAMRL_sub_noRI_results.Rdata', modelstring = "fitSub('S1',.15,'S2',.4,'R',3,'R_cor',6,'O1',1.2, 'showfigs','Off','RI',false);" )
+# model <- list(fname = 'SAMRL_sub_P_results.Rdata', modelstring = "fitSub('S1x1',.15/(1 +.15),'S1x2', .15/(1.6 +.15),'S2x2', .4/(1.2 +.4),'R',3/(3+1),'R_cor',6/(6+1),'free_params',{'S1x1','S1x2','S2x2','R','R_cor'}, 'showfigs','Off');")
+# model <- list(fname = 'SAMRL_sub_P_noRI_results.Rdata', modelstring = "fitSub('S1x1',.15/(1 +.15),'S1x2', .15/(1.6 +.15),'S2x2', .4/(1.2 +.4),'R',(3/3+1),'R_cor',(6/6+1),'free_params',{'S1x1','S1x2','S2x2','R','R_cor'}, 'showfigs','Off','RI',false);")
+
+## @knitr readAndScore
 if(.Platform$OS.type == "unix") {
   wd <- file.path("opt","source","RPmodels","H_and_H_LB4L","SAMRL")
   
@@ -13,19 +18,25 @@ library(dplyr)
 library(ggplot2)
 library(reshape2)
 library(R.matlab)
+library(boot)
 source('plotUtils.R')
 
 ## Fit the data ####
-if (!file.exists('SAMRL_sub_results.Rdata')) {
-  results <- fit('SAMRL_sub_results.Rdata', "fitSub('S1',.15,'S2',.4,'R',3,'R_cor',6,'O1',1.2, 'showfigs','Off');")
+if (!file.exists(model[[1]])) {
+  results <- fit(model[[1]], model[[2]])
 } else {
-  load('SAMRL_sub_results.Rdata')
+  load(model[[1]])
 }
 
 ## prep the data #####
 subfits <- results$fits
 sub_params <- as.data.frame(results$params)
-colnames(sub_params) <- c("O","R","Rcor","S1","S2")
+if (all(sub_params[1,] <=1) && all(sub_params[1,] >= 0 )) {
+  colnames(sub_params) <- c("R","Rcor","S1x1","S1x2","S2x2")
+} else {
+  colnames(sub_params) <- c("O","R","Rcor","S1","S2")
+}
+
 sub_ll <- results$err
 subfits$timepoint <- factor(subfits$timepoint, levels = c(1,2,3), labels = c("Practice Test", "Immediate Test", "Delayed Test"))
 subfits$other_type <- factor(subfits$other_type, levels = c('T', 'C',NA_character_), exclude = NULL)
@@ -34,6 +45,8 @@ subfits <- melt(subfits, id.vars = c("subject","group","timepoint", "practice", 
                 measure.vars = c("acc","pred_acc","acc_plus","pred_acc_plus","acc_neg","pred_acc_neg"),
                 value.name = 'pc')
 subfits$chain[subfits$chain==4] =1 
+subfits$LO <- log(subfits$pc/(1-subfits$pc))
+subfits$LO[is.nan(subfits$LO) | !is.finite(subfits$LO)] <- NA
 
 del = unique(subfits$subject[subfits$group=='delay'])
 imm = unique(subfits$subject[subfits$group=='immediate'])
@@ -73,10 +86,20 @@ for (j in c('immediate','delay')) {
 ## @knitr means
 sf_means1 <- subfits %>% filter(variable %in% c('acc','pred_acc'), !is.nan(pc)) %>%
   group_by(group,timepoint,practice,other_type,chain,variable) %>%
-  summarise(M = mean(pc)) 
+  summarise(M = mean(pc),
+            MLO = mean(LO,na.rm=TRUE),
+            semLO = sd(LO,na.rm=TRUE)/sqrt(n()),
+            upper = MLO + semLO,
+            lower = MLO - semLO)
+sf_means1 <- mutate_each(sf_means1, funs(inv.logit), MLO,semLO,upper,lower )
 sf_means2 <- subfits %>% filter(variable %in% c("acc_plus","pred_acc_plus","acc_neg","pred_acc_neg"), !is.nan(pc)) %>%
   group_by(group,timepoint,practice,other_type,chain,variable) %>%
-  summarise(cM =mean(pc)) 
+  summarise(cM =mean(pc),
+            MLO = mean(LO,na.rm=TRUE),
+            semLO = sd(LO,na.rm=TRUE)/sqrt(n()),
+            upper = MLO + semLO,
+            lower = MLO - semLO)
+sf_means2 <- mutate_each(sf_means2, funs(inv.logit), MLO,semLO,upper,lower )
 sf_means2$other_type <- factor(sf_means2$other_type)
 sf_means2$variable <- factor(sf_means2$variable,levels=c("acc_plus","acc_neg","pred_acc_plus","pred_acc_neg"))
 p1 <- ggplot(data=sf_means1,aes(x=timepoint,y=M, shape = variable,color = as.factor(chain), ymax=.85,ymin=.15)) +
@@ -84,6 +107,7 @@ p1 <- ggplot(data=sf_means1,aes(x=timepoint,y=M, shape = variable,color = as.fac
   #  geom_line(data=sf_means1[sf_means1$timepoint %in% c("Delayed Test", "Practice Test"),],  aes(group=interaction(chain,variable))) + 
   geom_line(data=sf_means1[sf_means1$timepoint %in% c("Delayed Test", "Immediate Test"),],  aes(group=interaction(chain,variable))) +
   geom_line(data=sf_means1[sf_means1$timepoint %in% c("Practice Test", "Immediate Test"),],  aes(group=interaction(group,chain,variable))) +   
+  geom_errorbar(aes(ymin = lower, ymax = upper),width=.1) + 
   scale_x_discrete("Test (Nested within Group)",expand=c(0,.25)) + 
   scale_color_discrete("Condition",labels = c("Practice Test",
                                               "Test Practice with Cue 1,\nFinal Test with Unpracticed Cue 2",
@@ -96,8 +120,14 @@ p1 <- ggplot(data=sf_means1,aes(x=timepoint,y=M, shape = variable,color = as.fac
   ggtitle(paste('Averaged Data and Model Predictions'))
 
 p2 <- ggplot(mapping= aes(x=other_type,y=cM,fill=variable)) +
-  geom_bar(data=sf_means2[sf_means2$variable %in% c("acc_plus","acc_neg"),], position='dodge',stat="identity") +
-  geom_point(data=sf_means2[sf_means2$variable %in% c("pred_acc_plus","pred_acc_neg"),], position= position_dodge(width=.9), stat="identity",shape = 4,size = 3.5) +
+  geom_bar(data=sf_means2[sf_means2$variable %in% c("acc_plus","acc_neg"),], 
+           position='dodge',stat="identity") +
+  geom_point(data=sf_means2[sf_means2$variable %in% c("pred_acc_plus","pred_acc_neg"),],
+             position= position_dodge(width=.9),shape = 4,size = 3.5) +
+  geom_errorbar(data=sf_means2[sf_means2$variable %in% c("acc_plus","acc_neg"),],
+                aes(ymin = lower, ymax = upper),position = position_dodge(width = .9),width=.2) +
+  geom_errorbar(data=sf_means2[sf_means2$variable %in% c("pred_acc_plus","pred_acc_neg"),],
+                aes(ymin = lower, ymax = upper),position = position_dodge(width = .9),width=.2) +
   scale_x_discrete("Retreival Cue Used", limits = c("Same Cue","New Cue")) + 
   scale_fill_brewer("Practice\nAccuracy", 
                     limits=c('acc_neg','acc_plus'), 
@@ -111,7 +141,7 @@ p2 <- ggplot(mapping= aes(x=other_type,y=cM,fill=variable)) +
 sf_par_means_imm <- colMeans(sub_params_imm)
 sf_par_means_del <- colMeans(sub_params_del)
 
-sf_chi_means_imm <- mean(sub_ll_imm)
-sf_chi_means_del <- mean(sub_ll_del)
+sf_ll_means_imm <- mean(sub_ll_imm)
+sf_ll_means_del <- mean(sub_ll_del)
 
 
