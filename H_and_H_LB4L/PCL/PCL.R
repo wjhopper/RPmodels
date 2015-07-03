a4085fc4f6ec2068d15564e15c1fe5827d4b2130
whopper <wjhopper510@gmail.com>
2015-07-03 12:48:10 -0700

    removed unneccesary reshaping of rbinom output to matrix format in PCLss, fixed a few parameter initialization problems.

--- H_and_H_LB4L/PCL/PCL.R
+++ H_and_H_LB4L/PCL/PCL.R
@@ -25,7 +25,7 @@ LL <- function(obs,pred,N) {
   return(err)
 }
 
-PCL <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1,F2=.1), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=15,Time=NULL),
+PCL <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1,F2=.1), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=15,Time=10),
                    data=NULL, fitting=FALSE) {
   
   p <- c(free,fixed)
@@ -46,7 +46,8 @@ PCL <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1,F2=.1), fixed = c(Tmin=1, Tm
   init_mem_C1 <- matrix(rbinom(mxn,p['nFeat'], p['ER']),nrow=p['nSim'],ncol=p['nList'])
   init_mem_C2 <- matrix(rbinom(mxn,p['nFeat'], p['ER']),nrow=p['nSim'],ncol=p['nList'])
   init_thresh <- matrix(rbinom(mxn,p['nFeat'], p['theta']),nrow=p['nSim'],ncol=p['nList'])
-  prac <- recall(init_mem_C1,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
+  prac_C1 <- recall(init_mem_C1,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
+  prac_C2 <- recall(init_mem_C2,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
   
   #control no practice
   #imm
@@ -70,38 +71,38 @@ PCL <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1,F2=.1), fixed = c(Tmin=1, Tm
   testStrengths <- init_mem_C1 
   testThresh <- init_thresh
   #imm
-  testStrengths[prac==TRUE] <- init_mem_C1[prac==TRUE] + matrix(rbinom(mxn,p['nFeat']-init_mem_C1, p['LR']),nrow=p['nSim'],ncol=p['nList'])[prac==TRUE]
-  testThresh[prac==TRUE] <- init_thresh[prac==TRUE] - matrix(rbinom(mxn,init_thresh, p['TR']),nrow=p['nSim'],ncol=p['nList'])[prac==TRUE]
+  testStrengths[prac_C1==TRUE] <- init_mem_C1[prac_C1==TRUE] + matrix(rbinom(mxn,p['nFeat']-init_mem_C1, p['LR']),nrow=p['nSim'],ncol=p['nList'])[prac_C1==TRUE]
+  testThresh[prac_C1==TRUE] <- init_thresh[prac_C1==TRUE] - matrix(rbinom(mxn,init_thresh, p['TR']),nrow=p['nSim'],ncol=p['nList'])[prac_C1==TRUE]
   testImmStrengths <- testStrengths - matrix(rbinom(p['nSim']*p['nList'],testStrengths, p['F1']),nrow=p['nSim'],ncol=p['nList'])
   testImmAcc <- recall(testImmStrengths, testThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
-  testImmAccPlus <- testImmAcc[prac==TRUE]
-  testImmAccNeg <- testImmAcc[prac==FALSE]
+  testImmAccPlus <- testImmAcc[prac_C1==TRUE]
+  testImmAccNeg <- testImmAcc[prac_C1==FALSE]
   #del
   testDelStrengths <- testStrengths - matrix(rbinom(mxn,testStrengths, p['F2']),nrow=p['nSim'],ncol=p['nList'])
   testDelAcc <- recall(testDelStrengths, testThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
-  testDelAccPlus <- testDelAcc[prac==TRUE]
-  testDelAccNeg <- testDelAcc[prac==FALSE]
+  testDelAccPlus <- testDelAcc[prac_C1==TRUE]
+  testDelAccNeg <- testDelAcc[prac_C1==FALSE]
   
   #no practice, other cue test practice
   #imm
   testOCImmStrengths <- init_mem_C2 - matrix(rbinom(mxn, init_mem_C2, p['F1']),nrow=p['nSim'],ncol=p['nList'])
   testOCImmThresh <- testThresh
   testOCImmAcc <- recall(testOCImmStrengths, testOCImmThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
-  testOCImmAccPlus <- testOCImmAcc[prac==TRUE]
-  testOCImmAccNeg <- testOCImmAcc[prac==FALSE]
+  testOCImmAccPlus <- testOCImmAcc[prac_C1==TRUE]
+  testOCImmAccNeg <- testOCImmAcc[prac_C1==FALSE]
   #del
   testOCDelStrengths <-controlDelStrengths
   testOCDelAcc <- recall(testOCDelStrengths, testOCImmThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
-  testOCDelAccPlus <- testOCDelAcc[prac==TRUE]
-  testOCDelAccNeg <- testOCDelAcc[prac==FALSE]
+  testOCDelAccPlus <- testOCDelAcc[prac_C1==TRUE]
+  testOCDelAccNeg <- testOCDelAcc[prac_C1==FALSE]
 
-  avgsImm <- lapply(list(prac=prac, chain1 = testOCImmAcc, chain1plus =  testOCImmAccPlus, chain1neg = testOCImmAccNeg,
+  avgsImm <- lapply(list(prac_C1=prac_C1, chain1 = testOCImmAcc, chain1plus =  testOCImmAccPlus, chain1neg = testOCImmAccNeg,
                          chain2 = controlImmAcc, chain3= restudyImmAcc,
                          chain5 = testImmAcc, chain5plus= testImmAccPlus,chain5neg = testImmAccNeg), mean)
   avgsDel<- lapply(list(chain1 = testOCDelAcc, chain1plus =  testOCImmAccPlus, chain1neg = testOCImmAccNeg,
                         chain2 = controlDelAcc, chain3= restudyDelAcc,
                         chain5 = testDelAcc, chain5plus= testDelAccPlus,chain5neg = testDelAccNeg ), mean)
-  data$pred_acc[!is.na(data$acc) & data$timepoint ==1] <- avgsImm$prac
+  data$pred_acc[!is.na(data$acc) & data$timepoint ==1] <- avgsImm$prac_C1
   data[data$chain==1 & data$timepoint >1,c("pred_acc", "pred_acc_plus","pred_acc_neg")] <- c(avgsImm$chain1, avgsImm$chain1plus,avgsImm$chain1neg, 
                                                                                              avgsDel$chain1,avgsDel$chain1plus,avgsDel$chain1plus)
   data$pred_acc[data$chain==2 & data$timepoint >1] <- c(avgsImm$chain2, avgsDel$chain2)
@@ -116,7 +117,7 @@ PCL <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1,F2=.1), fixed = c(Tmin=1, Tm
   }
 }
 
-PCLss <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=NULL,Time=NULL),
+PCLss <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=15,Time=10),
                           data=NULL, fitting=FALSE) {
   
   p <- c(free,fixed)
@@ -138,49 +139,48 @@ PCLss <- function(free= c(ER=.58,LR=.07,TR =.1, F1=.1), fixed = c(Tmin=1, Tmax=1
   init_mem_C1 <- matrix(rbinom(mxn,p['nFeat'], p['ER']),nrow=p['nSim'],ncol=p['nList'])
   init_mem_C2 <- matrix(rbinom(mxn,p['nFeat'], p['ER']),nrow=p['nSim'],ncol=p['nList'])
   init_thresh <- matrix(rbinom(mxn,p['nFeat'], p['theta']),nrow=p['nSim'],ncol=p['nList'])
-  prac <- recall(init_mem_C1,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
+  prac_C1 <- recall(init_mem_C1,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
+  prac_C2 <- recall(init_mem_C2,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
   
   #control no practice
-  controlImmStrengths <- init_mem_C1 - matrix(rbinom(mxn, init_mem_C1, p['F1']),nrow=p['nSim'],ncol=p['nList'])
+  controlImmStrengths <- init_mem_C1 - rbinom(mxn, init_mem_C1, p['F1'])
   controlImmAcc <- recall(controlImmStrengths, init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
 
   # study practice
   #imm
-  restudyStrengths <- init_mem_C1 + matrix(rbinom(mxn,p['nFeat']-init_mem_C1, p['LR']),nrow=p['nSim'],ncol=p['nList'])
-  restudyImmStrengths  <- restudyStrengths - matrix(rbinom(mxn,restudyStrengths, p['LR']),nrow=p['nSim'],ncol=p['nList'])
+  restudyStrengths <- init_mem_C1 + rbinom(mxn,p['nFeat']-init_mem_C1, p['LR'])
+  restudyImmStrengths  <- restudyStrengths - rbinom(mxn,restudyStrengths, p['LR'])
   restudyImmAcc<-recall(restudyImmStrengths, init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
   
   # test practice
-  #copy strengths and thresholds from practice test 
-  testStrengths <- init_mem_C1 
-  testThresh <- init_thresh
+  testStrengths <- init_mem_C1 #copy strengths and thresholds from practice test 
+  testThresh <- init_thresh 
   testImmAccPlus <- Matrix(0,nrow=p['nSim'],ncol=p['nList'], sparse = TRUE) # careful, is of class Matrix, not matrix
-  testImmAccNeg <- Matrix(0,nrow=p['nSim'],ncol=p['nList'], sparse = TRUE)
+  testImmAccNeg <- testImmAccPlus
   #imm
-  testStrengths[prac==TRUE] <- init_mem_C1[prac==TRUE] + matrix(rbinom(mxn,p['nFeat']-init_mem_C1, p['LR']),nrow=p['nSim'],ncol=p['nList'])[prac==TRUE]
-  testThresh[prac==TRUE] <- init_thresh[prac==TRUE] - matrix(rbinom(mxn,init_thresh, p['TR']),nrow=p['nSim'],ncol=p['nList'])[prac==TRUE]
-  testImmStrengths <- testStrengths - matrix(rbinom(p['nSim']*p['nList'],testStrengths, p['F1']),nrow=p['nSim'],ncol=p['nList'])
+  testStrengths[prac_C1==TRUE] <- init_mem_C1[prac_C1==TRUE] + rbinom(sum(prac_C1==TRUE),p['nFeat']-init_mem_C1[prac_C1==TRUE], p['LR'])
+  testThresh[prac_C1==TRUE] <- init_thresh[prac_C1==TRUE] - rbinom(sum(prac_C1==TRUE),init_thresh[prac_C1==TRUE], p['TR'])
+  testImmStrengths <- testStrengths -rbinom(mxn,testStrengths, p['F1'])
   testImmAcc <- recall(testImmStrengths, testThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
-  testImmAccPlus[prac==TRUE] <- testImmAcc[prac==TRUE]
-  testImmAccNeg[prac==FALSE] <- testImmAcc[prac==FALSE]
+  testImmAccPlus[prac_C1==TRUE] <- testImmAcc[prac_C1==TRUE]
+  testImmAccNeg[prac_C1==FALSE] <- testImmAcc[prac_C1==FALSE]
   
   #no practice, other cue test practice
-  #imm
-  testOCImmStrengths <- init_mem_C2 - matrix(rbinom(mxn, init_mem_C2, p['F1']),nrow=p['nSim'],ncol=p['nList'])
-  testOCImmThresh <- testThresh
+  testOCImmStrengths <- init_mem_C2 - rbinom(mxn, init_mem_C2, p['F1'])
   testOCImmAccPlus <- Matrix(0,nrow=p['nSim'],ncol=p['nList'], sparse = TRUE) # careful, is of class Matrix, not matrix
-  testOCImmAccNeg <- Matrix(0,nrow=p['nSim'],ncol=p['nList'], sparse = TRUE)
-  testOCImmAcc<- recall(testOCImmStrengths, testOCImmThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
-  testOCImmAccPlus[prac==TRUE] <- testOCImmAcc[prac==TRUE] 
-  testOCImmAccNeg[prac==FALSE] <- testOCImmAcc[prac==FALSE]
+  testOCImmAccNeg <-testOCImmAccPlus
+  #imm
+  testOCImmAcc<- recall(testOCImmStrengths, testThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
+  testOCImmAccPlus[prac_C2==TRUE] <- testOCImmAcc[prac_C2==TRUE] 
+  testOCImmAccNeg[prac_C2==FALSE] <- testOCImmAcc[prac_C2==FALSE]
   
   #compute predictions
-  avgs <- lapply(list(prac=prac, chain1plus = testOCImmAccPlus,chain1neg = testOCImmAccNeg, chain2 = controlImmAcc, chain3= restudyImmAcc,
+  avgs <- lapply(list(prac_C1=prac_C1, chain1plus = testOCImmAccPlus,chain1neg = testOCImmAccNeg, chain2 = controlImmAcc, chain3= restudyImmAcc,
                       chain5plus = testImmAccPlus,chain5neg = testImmAccNeg), mean)
-  avgs$chain1 <- (avgs$chain1plus*avgs$prac)+(avgs$chain1neg*(1-avgs$prac))
-  avgs$chain5 <- (avgs$chain5plus*avgs$prac)+(avgs$chain5neg*(1-avgs$prac))
+  avgs$chain1 <- (avgs$chain1plus*avgs$prac_C1)+(avgs$chain1neg*(1-avgs$prac_C1))
+  avgs$chain5 <- (avgs$chain5plus*avgs$prac_C1)+(avgs$chain5neg*(1-avgs$prac_C1))
   # Fill in data frame with preds
-  data$pred_acc[!is.na(data$acc) & data$timepoint==1]  <- avgs$prac
+  data$pred_acc[!is.na(data$acc) & data$timepoint==1]  <- avgs$prac_C1
   data[data$chain==1 & data$timepoint!=1, c("pred_acc", "pred_acc_plus","pred_acc_neg")] <- avgs[c("chain1", "chain1plus","chain1neg")]
   data$pred_acc[data$chain==2 & data$timepoint!=1] <- avgs$chain2
   data$pred_acc[data$chain==3 & data$timepoint!=1] <- avgs$chain3
