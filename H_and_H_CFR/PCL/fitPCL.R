80f47fb6b98a81fd42519836ccc90c84a1bf656b
whopper-sculder <wjhopper510@gmail.com>
2015-07-17 08:32:46 -0700

    I think I got breakpoint setting to work, and with wayyy simpler logic

--- H_and_H_CFR/PCL/fitPCL.R
+++ H_and_H_CFR/PCL/fitPCL.R
@@ -31,6 +31,12 @@ fitPCL <- function(model=1,inpar = FALSE,...,debugLevel = 0) {
     setwd(wd)
   }
   source('PCL.R')
+  
+  if (debugLevel[1]>0){
+#     trace(PCL,browser,debugLevel[2])
+    setBreakpoint('PCL.R', debugLevel[2], envir = fitPCL)
+  }
+  
   SS_data <- cbind(read.csv(file.path('..','CFRss.csv')),pred_acc = NA,pred_acc_plus= NA,pred_acc_neg= NA)
   SS_data <- filter(SS_data,!is.na(order))
   SS_data$RTrounded <- round(SS_data$RT,1)
@@ -60,51 +66,43 @@ fitPCL <- function(model=1,inpar = FALSE,...,debugLevel = 0) {
     }
   }
   
-  
-  k=1
-  if (debugLevel[1] == 0 || debugLevel[1]==2 ) {
-    if (inpar) {
-      writeLines(paste('[',Sys.time(),']',"INIT parlog"), con=file.path(wd,"parlog.txt"),sep='\n')
-      clusterExport(cl,c("recall","LL","RTdis"))
-      results <- foreach(m=models[model],export="wd") %:%
-        foreach(j =unique(m$data$sub_num),.verbose=T,.packages=c("dplyr","optimx","reshape2")) %dopar% {
-          sink(file.path(wd,"parlog.txt"), append=TRUE)
-          cat(paste("Fitting subject", j,"\n"))
-          optimx(par=m$free, fn = m$fcn, method = "Nelder-Mead",lower=m$low, upper=m$up,
-                 fixed=m$fix, data=m$data[m$data$sub_num ==j,], fitting=TRUE)
-        }
-      for (i in 1:length(results)){
-        m <- model[i]
-        models[[m]]$results <- results[[i]]
+  if (inpar) {
+    writeLines(paste('[',Sys.time(),']',"INIT parlog"), con=file.path(wd,"parlog.txt"),sep='\n')
+    clusterExport(cl,c("recall","LL","RTdis"))
+    results <- foreach(m=models[model],export="wd") %:%
+      foreach(j =unique(m$data$sub_num),.verbose=T,.packages=c("dplyr","optimx","reshape2")) %dopar% {
+        sink(file.path(wd,"parlog.txt"), append=TRUE)
+        cat(paste("Fitting subject", j,"\n"))
+        optimx(par=m$free, fn = m$fcn, method = "Nelder-Mead",lower=m$low, upper=m$up,
+               fixed=m$fix, data=m$data[m$data$sub_num ==j,], fitting=TRUE)
       }
-      stopCluster(cl)
-      cat(paste('[',Sys.time(),']',"Finished Fitting, Goodbye"))
-    } else {
-      for (i in model) {
-        for (j in unique(models[[i]]$data$sub_num)) {
-          message(paste("Fitting subject", j))
-          a <- optimx(par=models[[i]]$free, fn = models[[i]]$fcn, method = "Nelder-Mead",lower=models[[i]]$low, upper=models[[i]]$up,
-                      fixed=models[[i]]$fix, data=models[[i]]$data[models[[i]]$data$sub_num ==j,], fitting=TRUE)
-          models[[i]]$results[[k]] <- a
-          k=k+1
-        }
-      }
+    for (i in 1:length(results)){
+      m <- model[i]
+      models[[m]]$results <- results[[i]]
     }
-  } else if (debugLevel[1] == 1) {
+    stopCluster(cl)
+    cat(paste('[',Sys.time(),']',"Finished Fitting, Goodbye"))
+  } else {
+    k=1
     for (i in model) {
       for (j in unique(models[[i]]$data$sub_num)) {
-        message(paste("Testing Subject", j, ": model", names(models[i])))
-        res <- models[[i]]$fcn(free=models[[i]]$free,fixed=models[[i]]$fix,data=models[[i]]$data[models[[i]]$data$sub_num ==j,], fitting=FALSE)
-        models[[i]]$results[[k]] <- res
+        message(paste("Fitting subject", j))
+#         res <- models[[i]]$fcn(free=models[[i]]$free,fixed=models[[i]]$fix,data=models[[i]]$data[models[[i]]$data$sub_num ==j,], fitting=FALSE)
+        a <- optimx(par=models[[i]]$free, fn = models[[i]]$fcn, method = "Nelder-Mead",lower=models[[i]]$low, upper=models[[i]]$up,
+                    fixed=models[[i]]$fix, data=models[[i]]$data[models[[i]]$data$sub_num ==j,], fitting=TRUE)
+        models[[i]]$results[[k]] <- a
         k=k+1
       }
     }
   }
+
   k=1
   for (m in models[model]) {
     save(m,file=paste(names(models[model[k]]),"_results.Rdata",sep=''))
     k=k+1
   }
-
+  if (debugLevel[1]>0){
+    untrace(PCL)
+  }
   return(models[model])
 }
