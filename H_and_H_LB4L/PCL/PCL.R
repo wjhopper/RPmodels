f15c334c8288fbc0788d8967b583896e3dc4b9da
whopper-cemnl <wjhopper510@gmail.com>
2015-07-24 14:48:13 -0400

    changed PCLss model to calculate and fit joint probablities, and use a space out parameter to 'lower the effort' of the model such that sometimes a word that could be recalled basd on strengths is randomly not

--- H_and_H_LB4L/PCL/PCL.R
+++ H_and_H_LB4L/PCL/PCL.R
@@ -1,35 +1,37 @@
-recallTime <- function(mem=NULL,thresh=NULL,Tmin=NULL,Tmax=NULL,Time=NULL,lambda=NULL) {
+recallTime <- function(mem=NULL,thresh=NULL,space=NULL,Tmin=NULL,Tmax=NULL,Time=NULL,lambda=NULL) {
   RT=Tmin + (Tmax-Tmin)*exp(-lambda*abs(mem-thresh))  
   recalled <- (RT < Time) & (mem > thresh)
+  spaced <- as.logical(rbinom(recalled,1,space))
+  recalled[recalled & spaced] <- FALSE
   return(recalled)
 }
 
-recallNoTime <- function(mem=NULL,thresh=NULL,...) {
+recallNoTime <- function(mem=NULL,thresh=NULL,space=NULL,...) {
   recalled <- mem > thresh
+  spaced <- as.logical(rbinom(recalled,1,space))
+  recalled[recalled & spaced] <- FALSE
   return(recalled)
 } 
 
 g2 <- function(obs,pred,N) {
-  pred[pred==0] <- 0.001 # & !is.nan(N[pred==0])]# 1/(2*N[pred==0])
-  pred[pred==1] <- 0.999
+#   pred[pred==0] <- 0.001 # & !is.nan(N[pred==0])]# 1/(2*N[pred==0])
+#   pred[pred==1] <- 0.999
   Lc <- obs*(log(pred)) + ((1-obs)*log(1-pred))
   Lu <- obs*(log(obs)) + ((1-obs)*log(1-obs))
   err <- -sum(2*N*(Lc-Lu))
-  #   print(pred)
-  #   print(err)
   return(err)
 }
 
 LL <- function(obs,pred,N) {
   obs = obs * N
-  pred[pred==0] <- 0.001 # & !is.nan(N[pred==0])]# 1/(2*N[pred==0])
-  pred[pred==1] <- 0.999
+#   pred[pred==0] <- 0.001 # & !is.nan(N[pred==0])]# 1/(2*N[pred==0])
+#   pred[pred==1] <- 0.999
   ll = (obs*log(pred))+((N-obs)*log(1-pred))
   err=-sum(ll[!is.nan(ll)])        
   return(err)
 }
 
-PCL <- function(free= c(ER=.58,LR=.07,TR =.4, F1=.1,F2=.1), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=15,Time=10),
+PCL <- function(free= c(ER=.58,LR=.07,TR =.4, F1=.1,F2=.1,space=.03), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=15,Time=10),
                    data=NULL, fitting=FALSE) {
   
   p <- c(free,fixed)
@@ -115,12 +117,12 @@ PCL <- function(free= c(ER=.58,LR=.07,TR =.4, F1=.1,F2=.1), fixed = c(Tmin=1, Tm
   }
 }
 
-PCLss <- function(free= c(ER=.58,LR=.07,TR =.4, F1=.1), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=15,Time=10),
+PCLss <- function(free= c(ER=.58,LR=.07,TR =.4, F1=.1,space=.03), fixed = c(Tmin=1, Tmax=10, lambda=.5,theta=.5,nFeat=100,nSim=1000,nList=15,Time=10),
                           data=NULL, fitting=FALSE) {
   
   p <- c(free,fixed)
 
-  if (any(p[names(p) %in% c("ER","LR","TR","F1","theta")] > 1) || any(p[names(p) %in% c("ER","LR","TR","F1","lambda","theta")] < 0)) {
+  if (any(p[names(p) %in% c("ER","LR","TR","F1","theta","space")] > 1) || any(p[names(p) %in% c("ER","LR","TR","F1","lambda","theta","space")] < 0)) {
     err <- 100000
     return(err)
   } 
@@ -137,18 +139,18 @@ PCLss <- function(free= c(ER=.58,LR=.07,TR =.4, F1=.1), fixed = c(Tmin=1, Tmax=1
   init_mem_C1 <- matrix(rbinom(mxn,p['nFeat'], p['ER']),nrow=p['nSim'],ncol=p['nList'])
   init_mem_C2 <- matrix(rbinom(mxn,p['nFeat'], p['ER']),nrow=p['nSim'],ncol=p['nList'])
   init_thresh <- matrix(rbinom(mxn,p['nFeat'], p['theta']),nrow=p['nSim'],ncol=p['nList'])
-  prac <- recall(init_mem_C1,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
+  prac <- recall(init_mem_C1,init_thresh, p['space'], p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
   
   
   #control no practice
   controlImmStrengths <- init_mem_C1 - rbinom(mxn, init_mem_C1, p['F1'])
-  controlImmAcc <- recall(controlImmStrengths, init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
+  controlImmAcc <- recall(controlImmStrengths, init_thresh, p['space'], p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
 
   # study practice
   #imm
   restudyStrengths <- init_mem_C1 + rbinom(mxn,p['nFeat']-init_mem_C1, p['LR'])
   restudyImmStrengths  <- restudyStrengths - rbinom(mxn,restudyStrengths, p['F1'])
-  restudyImmAcc<-recall(restudyImmStrengths, init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
+  restudyImmAcc<-recall(restudyImmStrengths, init_thresh, p['space'], p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
   
   # test practice
   testStrengths <- init_mem_C1 #copy strengths and thresholds from practice test 
@@ -157,33 +159,51 @@ PCLss <- function(free= c(ER=.58,LR=.07,TR =.4, F1=.1), fixed = c(Tmin=1, Tmax=1
   testStrengths[prac==TRUE] <- init_mem_C1[prac==TRUE] + rbinom(sum(prac==TRUE),p['nFeat']-init_mem_C1[prac==TRUE], p['LR'])
   testThresh[prac==TRUE] <- init_thresh[prac==TRUE] - rbinom(sum(prac==TRUE),init_thresh[prac==TRUE], p['TR'])
   testImmStrengths <- testStrengths -rbinom(mxn,testStrengths, p['F1'])
-  testImmAcc <- recall(testImmStrengths, testThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
+  testImmAcc <- recall(testImmStrengths, testThresh,p['space'],p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
   
   #no practice, other cue test practice
   #imm
   testOCImmStrengths <- init_mem_C2 - rbinom(mxn, init_mem_C2, p['F1'])
-  testOCImmAcc<- recall(testOCImmStrengths, testThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
+  testOCImmAcc<- recall(testOCImmStrengths, testThresh, p['space'], p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
   #compute predictions
   avgs <- lapply(list(prac=prac,
-                      chain1 = testOCImmAcc, chain1plus = testOCImmAcc[prac==TRUE],chain1neg = testOCImmAcc[prac==FALSE], 
+                      chain1 = testOCImmAcc, chain1plus = testOCImmAcc[!prac],chain1neg = testOCImmAcc[!prac], 
+                      chain1_prac_final = (prac & testOCImmAcc),
+                      chain1_prac_not_final = (prac & !testOCImmAcc),
+                      chain1_not_prac_final = (!prac & testOCImmAcc),
+                      chain1_not_prac_not_final = (!prac & !testOCImmAcc),
                       chain2 = controlImmAcc, chain3= restudyImmAcc,
-                      chain5 = testImmAcc, chain5plus = testImmAcc[prac==TRUE],chain5neg = testImmAcc[prac==FALSE]),
+                      chain5 = testImmAcc, chain5plus = testImmAcc[prac],chain5neg = testImmAcc[!prac],
+                      chain5_prac_final = (prac & testImmAcc),
+                      chain5_prac_not_final = (prac & !testImmAcc),
+                      chain5_not_prac_final = (!prac & testImmAcc),
+                      chain5_not_prac_not_final = (!prac & !testImmAcc)),
                  mean)
   # Fill in data frame with preds
-  data$pred_acc[!is.na(data$acc) & data$timepoint==1]  <- avgs$prac
-  data[data$chain==1 & data$timepoint!=1, c("pred_acc", "pred_acc_plus","pred_acc_neg")] <- avgs[c("chain1", "chain1plus","chain1neg")]
-  data$pred_acc[data$chain==2 & data$timepoint!=1] <- avgs$chain2
-  data$pred_acc[data$chain==3 & data$timepoint!=1] <- avgs$chain3
-  data[data$chain==5  & data$timepoint!=1, c("pred_acc", "pred_acc_plus","pred_acc_neg")] <- avgs[c("chain5", "chain5plus","chain5neg")]
-  preds <- c(data$pred_acc[!is.na(data$pred_acc) & data$timepoint ==1],
-             data$pred_acc[!is.na(data$pred_acc) & !data$chain %in% c(1,5) & data$timepoint !=1], 
-             data$pred_acc_plus[!is.na(data$pred_acc_plus)], 
-             data$pred_acc_neg[!is.na(data$pred_acc_neg)])
-  obs <- c(data$acc[!is.na(data$acc) & data$timepoint ==1],
-           data$acc[!is.na(data$acc) & !data$chain %in% c(1,5) & data$timepoint !=1], 
-           data$acc_plus[!is.na(data$acc_plus)], 
-           data$acc_neg[!is.na(data$acc_neg)])
-  err <- LL(obs=obs,pred=preds,N=c(rep(p['nList'], length(preds) - 4), data$nplus[!(is.na(data$nplus))], data$nneg[!(is.na(data$nneg))]))
+  data$pred_prac_acc[!is.na(data$prac_acc)]  <- avgs$prac
+  insertCols <- c("pred_final_acc", "pred_acc_plus","pred_acc_neg","pred_prac_and_final",
+                 "pred_prac_and_not_final","pred_not_prac_and_final","pred_not_prac_and_not_final")
+  data[data$chain==1, insertCols] <- avgs[c("chain1", "chain1plus","chain1neg",
+                                            "chain1_prac_final","chain1_prac_not_final",
+                                            "chain1_not_prac_final","chain1_not_prac_not_final")]
+  data$pred_final_acc[data$chain==2] <- avgs$chain2
+  data$pred_final_acc[data$chain==3] <- avgs$chain3
+  data[data$chain==5, insertCols] <- avgs[c("chain5", "chain5plus","chain5neg",
+                                            "chain5_prac_final","chain5_prac_not_final",
+                                            "chain5_not_prac_final","chain5_not_prac_not_final")]
+  
+  preds <- c(data$pred_prac_acc[!is.na(data$pred_prac_acc)], # practice accuracy
+             data$pred_final_acc[data$practice %in% c('C','S') & is.na(data$other_type)], # control and study practice
+             unlist(t(data[!is.na(data$pred_acc_plus),tail(insertCols,4)]), use.names = FALSE)) # items with test practice 
+  obs <- c(data$prac_acc[!is.na(data$pred_prac_acc)], # practice accuracy
+           data$final_acc[data$practice %in% c('C','S') & is.na(data$other_type)], # control and study practice
+           unlist(t(data[!is.na(data$pred_acc_plus),gsub("pred_","",tail(insertCols,4))]), use.names = FALSE))# items with test practice 
+  N <- c(data$n[!is.na(data$pred_prac_acc)], # control and study practice
+         data$n[data$practice %in% c('C','S') & is.na(data$other_type)], # items with test practice 
+         unlist(t(data$n[!is.na(data$pred_acc_plus)]*data[!is.na(data$pred_acc_plus),
+                                                          gsub("pred_","",tail(insertCols,4))]), 
+                use.names = FALSE))
+  err <- LL(obs=obs,pred=preds,N=N)
   if (fitting) {
     return(err)
   } else {
