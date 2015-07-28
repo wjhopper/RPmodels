050aa3c381e5c8f5e8adde5501cf00a0750e23ce
whopper-cemnl <wjhopper510@gmail.com>
2015-07-28 08:48:08 -0400

    fixed bugs where unrecoverable items were not being filtered out of the vector that density was calcuated on, and now density is only calculated if there are more than 2 data points in that vector

--- H_and_H_CFR/PCL/PCL.R
+++ H_and_H_CFR/PCL/PCL.R
@@ -7,21 +7,21 @@ recall <- function(mem,thresh,Tmin=NULL,Tmax=NULL,Time=NULL,lambda=NULL) {
     }
   RT<- Tmin + (Tmax-Tmin)*exp(-lambda*abs(mem-thresh))
   CRT <- t(apply(RT,1,cumsum))
-  # Time <- 90 
   recalled <- (CRT < Time) & (mem >= thresh) # only above threshold and shorter than alloted time are recoverable
   return(list(Acc=recalled,RTrounded=RT,CRT=CRT))
 }
 
-RTdis <- function(RT,Time) {
+RTdis <- function(RT = NULL, recalled = NULL, Time= NULL) {
   
   RTdis=matrix(0,nrow=10*Time,ncol=ncol(RT))  #for each rank ordering recall position (Nlist), the KS density at each 1/10 second of recall test period (TestTime)
-  nas <- is.na(RT)
   for (n in 1:ncol(RT)) {
-    RTs<- RT[!nas[,n],n]
-    D <- density(RTs,bw=1,n=900,from=.1,to=90)
-    height <- D$y
-    height <- height/sum(height)
-    RTdis[,n] <- (length(RTs)/nrow(RT))*height
+    RTs<- RT[recalled[,n],n]
+    if (length(RTs)>2) {
+      D <- density(RTs,bw=1,n=900,from=.1,to=90)
+      height <- D$y
+      height <- height/sum(height)
+      RTdis[,n] <- (length(RTs)/nrow(RT))*height
+    }
   }
   RTdist <- melt(RTdis/sum(RTdis),varnames=c("RTrounded","order"),value.name = "RTdist")
   RTdist$RTrounded <- RTdist$RTrounded/10
@@ -33,21 +33,18 @@ LL <- function(obs,pred) {
     summarise(likelihood=sum(RTdist))
   sum_frame$likelihood[sum_frame$likelihood == 0] <- (0.5)*.Machine$double.xmin
   err <- -sum(log(sum_frame$likelihood))
-#   if (is.infinite(err)) {#((0.5)*.Machine$double.xmax)is.na(err) || is.infinite(err)) {
-#     print(err)
-#   }
   return(err)
 }
 
 PCL <- function(free= c(ER=.53,LR=.1,TR =.05, FR=.05), fixed = c(Tmin=2, Tmax=10, lambda=.2,theta=.5,nFeat=100,nSim=1000,nList=15,Time=90),
                 yoke = NULL, data=NULL, fitting=FALSE) {
-  
   p <- c(free,fixed)
   
   if (any(p[names(p) %in% c("ER","LR","TR","FR","theta")] > 1) || any(p[names(p) %in% c("ER","LR","TR","FR","lambda","theta")] < 0)) {
     err <- 100000
     return(err)
   }
+  
   set.seed(456)
   mxn <-  p['nSim']*p['nList'] #dimensions precalculation 
   
@@ -57,13 +54,13 @@ PCL <- function(free= c(ER=.53,LR=.1,TR =.05, FR=.05), fixed = c(Tmin=2, Tmax=10
 #   init_mem <- as.matrix(read.csv('mem.csv'))
 #   init_thresh <- as.matrix(read.csv('theta.csv'))
   prac <- recall(init_mem,init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
-  prac$dist <- RTdis(prac$RT,p['Time']) 
+  prac$dist <- RTdis(prac$RT,prac$Acc, p['Time']) 
   
   # study practice
   restudyStrengths <- init_mem + rbinom(mxn,p['nFeat']-init_mem, p['LR'])
   restudyStrengths  <- restudyStrengths - rbinom(mxn,restudyStrengths, p['FR'])
   restudy<-recall(restudyStrengths, init_thresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])  
-  restudy$dist <- RTdis(restudy$RT,p['Time']) 
+  restudy$dist <- RTdis(restudy$RT,restudy$Acc,p['Time']) 
   
   # test practice
   testStrengths <- init_mem #copy strengths and thresholds from practice test 
@@ -73,7 +70,7 @@ PCL <- function(free= c(ER=.53,LR=.1,TR =.05, FR=.05), fixed = c(Tmin=2, Tmax=10
   testThresh[prac$Acc==TRUE] <- init_thresh[prac$Acc==TRUE] - rbinom(sum(prac$Acc==TRUE),init_thresh[prac$Acc==TRUE], p['TR'])
   testStrengths <- testStrengths -rbinom(mxn,testStrengths, p['FR'])
   test <- recall(testStrengths, testThresh, p['Tmin'], p['Tmax'], p['Time'],p['lambda'])
-  test$dist <- RTdis(test$RT,p['Time']) 
+  test$dist <- RTdis(test$RT,test$Acc,p['Time']) 
   
   dist <- data.frame(class = rep(c('np','sp','tp'),each=nrow(prac$dist)),
                       rbind(prac$dist, restudy$dist, test$dist))
